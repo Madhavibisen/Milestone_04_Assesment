26dcf4149bfe013d9e035112bee0233559b4af5e
Ashwini Bisen <madhavibisen95304@gmail.com>
2021-11-09 00:14:53 +0530

    Adding the files

--- src/test/java/com/mindtree/runner/TestRunner.java
+++ src/test/java/com/mindtree/runner/TestRunner.java
@@ -3,13 +3,13 @@ package com.mindtree.runner;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "Feature",
-		glue = "com.mindtree.stepdefinations")
+		glue = { "com.mindtree.stepdefinations"}, dryRun=false)
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
